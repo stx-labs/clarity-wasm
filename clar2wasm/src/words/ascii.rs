4012c9e1d7aa75a124a71cb6844dd467c87280af
Anthony Caccia <anthony@stackslabs.com>
2026-01-30 15:32:23 +0100

    feat: add to-ascii for uint

--- clar2wasm/src/words/ascii.rs
+++ clar2wasm/src/words/ascii.rs
@@ -1,5 +1,6 @@
 use clarity_types::types::{SequenceSubtype, StringSubtype, TypeSignature};
-use walrus::ir::MemArg;
+use walrus::ir::{BinaryOp, MemArg, StoreKind, UnaryOp};
+use walrus::LocalId;
 
 use crate::check_args;
 use crate::wasm_generator::GeneratorError;
@@ -106,6 +107,225 @@ fn to_ascii_bool(
         // the err value is irrelevant for a bool argument
         .i64_const(0)
         .i64_const(0);
+
+    Ok(())
+}
+
+fn to_ascii_uint(
+    generator: &mut crate::wasm_generator::WasmGenerator,
+    builder: &mut walrus::InstrSeqBuilder,
+    _expr: &clarity::vm::SymbolicExpression,
+    arg: &clarity::vm::SymbolicExpression,
+) -> Result<(), crate::wasm_generator::GeneratorError> {
+    // the biggest uint we could write will have the length of u128::MAX: 39 characters.
+    // We also need a space for the character 'u'
+    let (offset, _len) = generator.create_call_stack_local(
+        builder,
+        &TypeSignature::new_ascii_type_checked(40),
+        false,
+        true,
+    );
+
+    let lo = generator.borrow_local(walrus::ValType::I64);
+    let hi = generator.borrow_local(walrus::ValType::I64);
+    let length = generator.borrow_local(walrus::ValType::I32);
+
+    generator.traverse_expr(builder, arg)?;
+    builder.local_set(*hi).local_set(*lo);
+
+    builder
+        .local_get(offset)
+        .i32_const(40)
+        .binop(BinaryOp::I32Add)
+        .local_set(offset);
+    builder.i32_const(0).local_set(*length);
+
+    to_ascii_u128(generator, builder, *lo, *hi, offset, *length)?;
+
+    // we write a 'u' in front of the result
+    builder
+        .local_get(offset)
+        .i32_const(1)
+        .binop(BinaryOp::I32Sub)
+        .local_tee(offset)
+        .i32_const(b'u' as i32)
+        .store(
+            generator.get_memory()?,
+            StoreKind::I32_8 { atomic: false },
+            MemArg {
+                align: 1,
+                offset: 0,
+            },
+        );
+
+    builder
+        .i32_const(1)
+        .local_get(offset)
+        // length + 1 for the character 'u'
+        .local_get(*length)
+        .i32_const(1)
+        .binop(BinaryOp::I32Add)
+        .i64_const(0)
+        .i64_const(0);
+
+    Ok(())
+}
+
+fn to_ascii_u128(
+    generator: &mut crate::wasm_generator::WasmGenerator,
+    builder: &mut walrus::InstrSeqBuilder,
+    lo: LocalId,
+    hi: LocalId,
+    offset: LocalId,
+    length: LocalId,
+) -> Result<(), crate::wasm_generator::GeneratorError> {
+    let memory = generator.get_memory()?;
+    // we make a first loop with the u128 division while hi > 0
+    builder
+        .local_get(hi)
+        .i64_const(0)
+        .binop(BinaryOp::I64Ne)
+        .if_else(
+            None,
+            |then| {
+                then.loop_(None, |loop_| {
+                    let loop_id = loop_.id();
+                    let div_128 = generator.func_by_name("stdlib.div-int128");
+
+                    // we update the offset at which the character should be written
+                    loop_
+                        .local_get(offset)
+                        .i32_const(1)
+                        .binop(BinaryOp::I32Sub)
+                        .local_set(offset);
+
+                    loop_
+                        .local_get(lo)
+                        .local_get(hi)
+                        .i64_const(10)
+                        .i64_const(0)
+                        .call(div_128);
+                    // drop the remainder hi
+                    loop_.drop();
+                    // we set the remainder_lo in lo converted to its ascii value
+                    loop_.i64_const(48).binop(BinaryOp::I64Add).local_set(lo);
+                    // we store it to the correct offset
+                    loop_.local_get(offset).local_get(lo).store(
+                        memory,
+                        StoreKind::I64_8 { atomic: false },
+                        MemArg {
+                            align: 1,
+                            offset: 0,
+                        },
+                    );
+
+                    // we store the new hi and lo from the stack
+                    loop_.local_set(hi).local_set(lo);
+
+                    // we update the result size
+                    loop_
+                        .local_get(length)
+                        .i32_const(1)
+                        .binop(BinaryOp::I32Add)
+                        .local_set(length);
+
+                    // we keep going through the slow loop while hi is > 0
+                    loop_
+                        .local_get(hi)
+                        .i64_const(0)
+                        .binop(BinaryOp::I64Ne)
+                        .br_if(loop_id);
+                });
+            },
+            |_else| {},
+        );
+
+    // we make a second loop using the faster i64 division when we have only lo
+    builder
+        .local_get(lo)
+        .i64_const(0)
+        .binop(BinaryOp::I64Ne)
+        .if_else(
+            None,
+            |then| {
+                then.loop_(None, |loop_| {
+                    let loop_id = loop_.id();
+
+                    // we update the offset at which the character should be written
+                    loop_
+                        .local_get(offset)
+                        .i32_const(1)
+                        .binop(BinaryOp::I32Sub)
+                        .local_tee(offset);
+
+                    // we compute (lo % 10) on the stack and set (lo % 10) using the formula
+                    // divmod(lo, 10) => { div = lo / 10 ; mod = (div * -10) + lo }
+                    loop_
+                        .local_get(lo)
+                        .local_get(lo)
+                        .i64_const(10)
+                        .binop(BinaryOp::I64DivU)
+                        .local_tee(lo)
+                        .i64_const(-10)
+                        .binop(BinaryOp::I64Mul)
+                        .binop(BinaryOp::I64Add);
+
+                    // we convert the value on stack to its ascii value
+                    loop_.i64_const(48).binop(BinaryOp::I64Add);
+
+                    // we store the value (offset already on stack)
+                    loop_.store(
+                        memory,
+                        StoreKind::I64_8 { atomic: false },
+                        MemArg {
+                            align: 1,
+                            offset: 0,
+                        },
+                    );
+
+                    // we update the result size
+                    loop_
+                        .local_get(length)
+                        .i32_const(1)
+                        .binop(BinaryOp::I32Add)
+                        .local_set(length);
+
+                    // we keep going through the slow loop while lo is > 0
+                    loop_
+                        .local_get(lo)
+                        .i64_const(0)
+                        .binop(BinaryOp::I64Ne)
+                        .br_if(loop_id);
+                });
+            },
+            |_else| {},
+        );
+
+    // we need to account for the printing of 0. We have 0 if we didn't enter any of the previous loops,
+    // which implies that the current value of length is also 0.
+    builder.local_get(length).unop(UnaryOp::I32Eqz).if_else(
+        None,
+        |then| {
+            then
+                // offset
+                .local_get(offset)
+                .i32_const(1)
+                .local_tee(length)
+                .binop(BinaryOp::I32Sub)
+                .local_tee(offset)
+                // value '0'
+                .i32_const(b'0' as i32)
+                .store(
+                    memory,
+                    StoreKind::I32_8 { atomic: false },
+                    MemArg {
+                        align: 1,
+                        offset: 0,
+                    },
+                );
+        },
+        |_else| {},
+    );
 
     Ok(())
 }
